233735a1e90bfc7f3b13b0c0173e105825540b4c
Kiehyun Kevin Park <guitar79@naver.com>
2017-11-22 16:26:54 +0900

    Add files via upload

--- AirKorea/Hourly_mariaDB_to_daily_mariaDB_02.py
+++ AirKorea/Hourly_mariaDB_to_daily_mariaDB_02.py
@@ -11,12 +11,9 @@
 
 
 import numpy as np
-from scipy import stats
-import pandas as pd
 import os
-from pathlib import Path
 np.set_printoptions(threshold=100)
-
+import multiprocessing as proc
 import pymysql
 import pymysql.cursors
 
@@ -24,9 +21,12 @@ import pymysql.cursors
 D=[0,31,28,31,30,31,30,31,31,30,31,30,31]
 
 #mariaDB info
+db_host = '10.114.0.121'
 db_user = 'root'
 db_pass = 'rudrlrhkgkrrh'
 db_name = 'AirKorea'
+tb_hourly = 'hourly_vc'
+tb_daily = 'daily_all_vc'
 
 #base directory
 drbase = '/media/guitar79/8T/RS_data/Remote_Sensing/2017RNE/airkorea/'
@@ -34,33 +34,38 @@ drout = 'daily_mean3/'
 if not os.path.exists(drbase+drout):
     os.makedirs(drbase+drout)
 #db connect
-conn= pymysql.connect(host='localhost',user=db_user,password=db_pass,db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
+conn= pymysql.connect(host=db_host,user=db_user,password=db_pass,db=db_name,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
 
 #all ocode read!!
-ocodecur = conn.cursor()
-ocodecur.execute("SELECT DISTINCT ocode FROM hourly")
-ocodes = ocodecur.fetchall()
+cur = conn.cursor()
+print("SELECT DISTINCT ocode from %s ORDER BY ocode ASC " %(tb_hourly))
+cur.execute("SELECT DISTINCT ocode from %s ORDER BY ocode ASC " %(tb_hourly))
+ocodes = cur.fetchall()
 #print(len(ocodes))
 res = []
 for i in range(len(ocodes)):
     res.append(ocodes[i]['ocode'])
 ocodes=res
-
 #ocodes = ocodes
 #cur.execute("SELECT DISTINCT ? FROM hourly", (ocode))
 
-for ocode in sorted(ocodes):
-    ocode = str(ocode)
+def check_none(x):
+    if np.isnan(x): return ""
+    return x
+
+def Work(all_rows, q):
+    pos = 0
     for year in range(2014,2017):
         for Mo in range(1,13):
             for Da in range(1,D[Mo]):
                 day=year*10000+Mo*100+Da
                 sdatetime = day*100
-                edatetime = day*100+100
-                #print(ocode,sdatetime,edatetime)
-                cur = conn.cursor()
-                cur.execute("SELECT * FROM hourly WHERE ocode=%s and otime>=%s and otime<%s", (ocode,sdatetime,edatetime))
-                rows = cur.fetchall()
+                edatetime = day*100+100 
+                while True:
+                    new_row = all_rows[pos]
+                    if new_row['otime']>edatetime:break
+                    else: rows.append(new_row)
+                    pos = pos + 1
                 if len(rows) == 0:
                     print('data is empty %s from %s to %s' %(ocode, sdatetime,edatetime))
                 else:
@@ -73,7 +78,7 @@ for ocode in sorted(ocodes):
                     #print(rows.shape)
                     #print(type(rows))
                     #s.sum(skipna=True)
-                    sumSO2 = np.sum(rows[:,4].astype(np.float))
+                    sumSO2 = check_none(np.sum(rows[:,4].astype(np.float)))
                     sumCO = np.sum(rows[:,5].astype(np.float))
                     sumO3 = np.sum(rows[:,6].astype(np.float))
                     sumNO2 = np.sum(rows[:,7].astype(np.float))
@@ -115,14 +120,63 @@ for ocode in sorted(ocodes):
                     minPM10 = np.min(rows[:,8].astype(np.float))
                     minPM25 = np.min(rows[:,9].astype(np.float))
                     
-                    print('%s,,%s,,sum,%s,%s,%s,%s,%s,%s,' % (day,ocode,sumSO2,sumCO,sumO3,sumNO2,sumPM10,sumPM25))
-                    cur.execute("INSERT INTO `AirKorea_parks`.`daily`(`id`, `day`, `ocode`, `type`, `SO2`, `CO`, `O3`, `NO2`, `PM10`, `PM2.5`, `remark`) \
-                                VALUES (NULL, %s, %s, 'sum', %s, %s, %s, %s, %s, %s, %s);" \
-                                %(day,ocode,sumSO2,sumCO,sumO3,sumNO2,sumPM10,sumPM25,len(rows)))
-                    cur.execute("INSERT INTO `AirKorea_parks`.`daily`(`id`, `day`, `ocode`, `type`, `SO2`, `CO`, `O3`, `NO2`, `PM10`, `PM2.5`, `remark`) \
-                                VALUES (NULL, %s, %s, 'mean', %s, %s, %s, %s, %s, %s, %s);" \
-                                %(day,ocode,meanSO2,meanCO,meanO3,meanNO2,meanPM10,meanPM25,len(rows)))
-                    conn.commit()
+                    #print('%s,,%s,,sum,%s,%s,%s,%s,%s,%s,' % (day,ocode,sumSO2,sumCO,sumO3,sumNO2,sumPM10,sumPM25))
+                    print("INSERT INTO %s.%s\
+                        (`id`, `day`, `ocode`,\
+                        `sumSO2`, `sumCO`, `sumO3`, `sumNO2`, `sumPM10`, `sumPM25`,\
+                        `meanSO2`, `meanCO`, `meanO3`, `meanNO2`, `meanPM10`, `meanPM25`,\
+                        `varSO2`, `varCO`, `varO3`, `varNO2`, `varPM10`, `varPM25`,\
+                        `stdSO2`, `stdCO`, `stdO3`, `stdNO2`, `stdPM10`, `stdPM25`,\
+                        `maxSO2`, `maxCO`, `maxO3`, `maxNO2`, `maxPM10`, `maxPM25`,\
+                        `minSO2`, `minCO`, `minO3`, `minNO2`, `minPM10`, `minPM25`,\
+                        `remark`)\
+                        VALUES (NULL, %s, %s,\
+                        %s, %s, %s, %s, %s, %s,\
+                        %s, %s, %s, %s, %s, %s,\
+                        %s, %s, %s, %s, %s, %s,\
+                        %s, %s, %s, %s, %s, %s,\
+                        %s, %s, %s, %s, %s, %s,\
+                        %s, %s, %s, %s, %s, %s,\
+                        %s);"\
+                        %(db_name, tb_daily, day, ocode,\
+                        sumSO2, sumCO, sumO3, sumNO2, sumPM10, sumPM25,\
+                        meanSO2, meanCO, meanO3, meanNO2, meanPM10, meanPM25,\
+                        varSO2, varCO, varO3, varNO2, varPM10, varPM25,\
+                        stdSO2, stdCO, stdO3, stdNO2, stdPM10, stdPM25,\
+                        maxSO2, maxCO, maxO3, maxNO2, maxPM10, maxPM25,\
+                        minSO2, minCO, minO3, minNO2, minPM10, minPM25,\
+                        len(rows)))
+                    ''' 
+                    cur.execute("INSERT INTO %s.%s\
+                        (`id`, `day`, `ocode`,\
+                        `sumSO2`, `sumCO`, `sumO3`, `sumNO2`, `sumPM10`, `sumPM25`,\
+                        `meanSO2`, `meanCO`, `meanO3`, `meanNO2`, `meanPM10`, `meanPM25`,\
+                        `varSO2`, `varCO`, `varO3`, `varNO2`, `varPM10`, `varPM25`,\
+                        `stdSO2`, `stdCO`, `stdO3`, `stdNO2`, `stdPM10`, `stdPM25`,\
+                        `maxSO2`, `maxCO`, `maxO3`, `maxNO2`, `maxPM10`, `maxPM25`,\
+                        `minSO2`, `minCO`, `minO3`, `minNO2`, `minPM10`, `minPM25`,\
+                        `remark`)\
+                        VALUES (NULL, %s, %s,\
+                        %s, %s, %s, %s, %s, %s,\
+                        %s, %s, %s, %s, %s, %s,\
+                        %s, %s, %s, %s, %s, %s,\
+                        %s, %s, %s, %s, %s, %s,\
+                        %s, %s, %s, %s, %s, %s,\
+                        %s, %s, %s, %s, %s, %s,\
+                        %s);"\
+                        %(db_name, tb_daily, day,ocode,\
+                        sumSO2, sumCO, sumO3, sumNO2, sumPM10, sumPM25,\
+                        meanSO2, meanCO, meanO3, meanNO2, meanPM10, meanPM25,\
+                        varSO2, varCO, varO3, varNO2, varPM10, varPM25,\
+                        stdSO2, stdCO, stdO3, stdNO2, stdPM10, stdPM25,\
+                        maxSO2, maxCO, maxO3, maxNO2, maxPM10, maxPM25,\
+                        minSO2, minCO, minO3, minNO2, minPM10, minPM25,\
+                        len(rows)))
+                    '''
+                        #cur.execute("INSERT INTO `AirKorea_parks`.`daily`(`id`, `day`, `ocode`, `type`, `SO2`, `CO`, `O3`, `NO2`, `PM10`, `PM2.5`, `remark`) \
+                        #VALUES (NULL, %s, %s, 'mean', %s, %s, %s, %s, %s, %s, %s);" \
+                        #%(day,ocode,meanSO2,meanCO,meanO3,meanNO2,meanPM10,meanPM25,len(rows)))
+                    #conn.commit()
                     #with open('%s%sdaily_%s.csv' %(drbase,drout,ocode), 'a') as o:
                         #print('%s,,%s,,sum,%s,%s,%s,%s,%s,%s,' % (day,ocode,sumSO2,sumCO,sumO3,sumNO2,sumPM10,sumPM25), file=o)
                         #print('%s,,%s,,mean,%s,%s,%s,%s,%s,%s,' % (day,ocode,meanSO2,meanCO,meanO3,meanNO2,meanPM10,meanPM25), file	=o)
@@ -130,3 +184,20 @@ for ocode in sorted(ocodes):
                         #print('%s,,%s,,std,%s,%s,%s,%s,%s,%s,' % (day,ocode,stdSO2,stdCO,stdO3,stdNO2,stdPM10,stdPM25), file=o)
                         #print('%s,,%s,,max,%s,%s,%s,%s,%s,%s,' % (day,ocode,maxSO2,maxCO,maxO3,maxNO2,maxPM10,maxPM25), file=o)
                         #print('%s,,%s,,min,%s,%s,%s,%s,%s,%s,' % (day,ocode,minSO2,minCO,minO3,minNO2,minPM10,minPM25), file=o)
+ 
+q = proc.Queue()
+P = []
+data = []
+print("Debug1")
+
+for i in range(len(ocodes)):
+    ocode = ocodes[i]
+    cur.execute("SELECT * from %s where ocode=%s ORDER BY otime ASC" %(tb_hourly, ocode))
+    all_data = cur.fetchall()
+    data.append(all_data)
+    print(len(data[i]))
+
+for i in range(len(ocodes)):
+    P.append(proc.Process(target=Work, args=(data[i],q)))
+    P[len(P)-1].start()
+
